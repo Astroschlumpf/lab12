f2171b9828999dcd5f8549d98598d0f4f4a9a80f
Oliver Hielscher <ohielscher@pc05.pool.tp1.uni-duesseldorf.de>
2016-02-02 15:50:17 +0100

    lab12

--- nlsi.cxx
+++ nlsi.cxx
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <complex>
 #include <sstream>
+#include <cmath>
 //-----------------------------------
 using namespace std;
 //-----------------------------------
@@ -11,7 +12,12 @@ void init( cmplx* const psi0, const double eta, const double sigma, const double
           const int Nx);
 
 void writeToFile(const cmplx* const v, const string s, const double dx,
-                 const int Nx, const double xmin);
+                 const int Nx, const double xmin, const double t);
+void stepL(cmplx* const f1, cmplx* const f0,
+          const double dt, const double dx, const int N);
+void stepN(cmplx* const f1, cmplx* const f0,
+          const double dt, const double dx, const int N);
+
 //-----------------------------------
 int main(){
 
@@ -25,40 +31,107 @@ int main(){
 	int Nk = int(Tend / Na / dt + 0.5);
 
 	const double eta = 0.2;
-
+        double t=0;
 	stringstream strm;
 
 	cmplx* psi0 = new cmplx[Nx];
+	cmplx* psi1 = new cmplx[Nx];
+	cmplx* h;
 
 	init(psi0, eta, dx, dt,Nx);
 
-	writeToFile(psi0,"psi_0", dx,Nx,xmin);
-
+	writeToFile(psi0,"psi_0", dx,Nx,xmin,t);
+// 	int i = 1;
 
 	for (int i = 1; i <= Na; i++) {
 
 		for (int j = 1; j <= Nk-1; j++) {
+		  stepL(psi1,psi0,dt,dx,Nx);
+		  stepN(psi1,psi0,dt,dx,Nx);
+		  h=psi0;
+		  psi0=psi1;
+		  psi1=h;
+		  t+=dt;
 		}
 		strm.str("");
 		strm << "psi_" << i;
-		writeToFile(psi0,strm.str(), dx,Nx,xmin);
+		writeToFile(psi0,strm.str(), dx,Nx,xmin,t);
 	}
 
 	return 0;
 }
+
+//-----------------------------------
+void stepL(cmplx* const f1, cmplx* const f0,
+          const double dt, const double dx, const int N)
+{
+
+  cmplx* d=new cmplx[N];
+  cmplx* u=new cmplx[N];
+  cmplx* l=new cmplx[N];
+ 
+  cmplx alpha = - cmplx(0.,dt/(dx*dx));
+  
+  for(int i=0;i<N;i++) d[i] = cmplx(1.0, -2.0*dt/(dx*dx));
+  for(int i=0;i<N;i++) u[i] = -alpha;
+  for(int i=0;i<N;i++) l[i] = -alpha;
+  
+//   for(int i=1;i<N;i++){
+//     d[i]=d[i]*d[i-1]-u[i-1]*l[i];
+//     u[i]*=d[i-1];
+//     f0[i]=f0[i]*d[i-1]-f0[i-1]*l[i];
+//   }
+//   f1[N-1]=f0[N-1]/d[N-1];
+//   for (int i=N-2;i>=0;i--){
+//     f1[i]=(f0[i]-u[i]*f1[i+1])/d[i];
+//   }
+
+
+  
+  u[0] = u[0] / d[0]; 
+  f0[0] = f0[0] / d[0];
+  d[0] = 1.0;
+  for(int i=1;i<N;i++){
+    cmplx faktor = 1.0/(d[i] -  (-alpha)/d[i-1]*u[i-1]);
+    d[i]  = 1;
+       
+    u[i] = u[i] * faktor;
+    f0[i] = ( f0[i] - (-alpha)/d[i-1]*f0[i-1] )  * faktor;
+  }
+  
+  
+   f1[N-1]=f0[N-1];
+    for (int i=N-2;i>=0;i--){
+      f1[i]=f0[i]-u[i]*f1[i+1];
+   }
+  
+  delete[] d;
+  delete[] u;
+  delete[] l;
+}
+
+//-----------------------------------
+void stepN(cmplx* const f1, cmplx* const f0,
+          const double dt, const double dx, const int N)
+{
+  for(int i = 0; i < N; i++){
+    f1[i] = f1[i] * exp(cmplx(0., - norm(f1[i]) * dt));
+  }
+}
+
 //-----------------------------------
 void writeToFile(const cmplx* const v, const string s, const double dx,
-                 const int Nx, const double xmin)
+                 const int Nx, const double xmin, const double t)
 {
 	ofstream out(s.c_str());
 	for(int i=0; i<Nx; i++){
-		double x = xmin + i * dx;
+		double x = xmin + i * dx + t*5;
 		out << x << "\t" << norm(v[i]) << "\t" << v[i].real() << "\t" << v[i].imag() << endl;
 	}
 	out.close();
 }
-//-----------------------------------
 
+//-----------------------------------
 void init( cmplx* const psi0, const double eta,  const double dx, const double dt,
           const int Nx)
 {
